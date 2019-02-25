6e280694b84e62a58773bd8a5abe8149e4418c8a
Constance <4barbes@gmail.com>
2019-02-25 21:21:10 +0100

    Correction offset

--- main.py
+++ main.py
@@ -27,49 +27,49 @@ parameters = {
         },
     "transition-ecologique-solidaire-agriculture-alimentation":
         {
-            "url": "https://le-vrai-debat.fr/project/transition-ecologique-solidaire-agriculture-alimentation/consultation/consultation-2/types/transition-ecologique-et-solidaire-agriculture-alimentation-transport/page/1",
+            "url": "https://le-vrai-debat.fr/project/transition-ecologique-solidaire-agriculture-alimentation/consultation/consultation-2/types/transition-ecologique-et-solidaire-agriculture-alimentation-transport",
             "totalCount": 3155,
             "pageNb": 316,
         },
     "justice-police-armee":
         {
-            "url": "https://le-vrai-debat.fr/project/justice-police-armee/consultation/consultation-3/types/justice-police-armee/page/1",
+            "url": "https://le-vrai-debat.fr/project/justice-police-armee/consultation/consultation-3/types/justice-police-armee",
             "totalCount": 1420,
             "pageNb": 142,
         },
     "europe-affaires-etrangeres-outre-mer":
         {
-            "url": "https://le-vrai-debat.fr/project/europe-affaires-etrangeres-outre-mer/consultation/consultation-4/types/europe-affaires-etrangeres-outre-mer/page/1",
+            "url": "https://le-vrai-debat.fr/project/europe-affaires-etrangeres-outre-mer/consultation/consultation-4/types/europe-affaires-etrangeres-outre-mer",
             "totalCount": 905,
             "pageNb": 91,
         },
     "sante-solidarite-handicap":
         {
-            "url": "https://le-vrai-debat.fr/project/europe-affaires-etrangeres-outre-mer/consultation/consultation-5/types/sante-solidarite-handicap/page/1",
+            "url": "https://le-vrai-debat.fr/project/europe-affaires-etrangeres-outre-mer/consultation/consultation-5/types/sante-solidarite-handicap",
             "totalCount": 1820,
             "pageNb": 182,
         },
     "economie-finances-travail-compte-public":
         {
-            "url":"https://le-vrai-debat.fr/project/economie-finances-travail-compte-public/consultation/consultation-6/types/economie-finances-travail-compte-public/page/1",
+            "url":"https://le-vrai-debat.fr/project/economie-finances-travail-compte-public/consultation/consultation-6/types/economie-finances-travail-compte-public",
             "totalCount":6146,
             "pageNb":615
         },
     "education-jeunesse-enseignement-superieur-recherche-et-innovation":
         {
-            "url":"https://le-vrai-debat.fr/project/education-jeunesse-enseignement-superieur-recherche-et-innovation/consultation/consultation-7/types/education-jeunesse-enseignement-superieur-recherche-et-innovation/page/1",
+            "url":"https://le-vrai-debat.fr/project/education-jeunesse-enseignement-superieur-recherche-et-innovation/consultation/consultation-7/types/education-jeunesse-enseignement-superieur-recherche-et-innovation",
             "totalCount":1507,
             "pageNb":151
         },
     "sport-culture":
         {
-            "url":"https://le-vrai-debat.fr/project/sport-culture/consultation/consultation-8/types/sport-culture/page/1",
+            "url":"https://le-vrai-debat.fr/project/sport-culture/consultation/consultation-8/types/sport-culture",
             "totalCount":566,
             "pageNb":57
         },
     "expression-libre":
         {
-            "url":"https://le-vrai-debat.fr/project/expression-libre/consultation/consultation-9/types/expression-libre-sujets-de-societe/page/1",
+            "url":"https://le-vrai-debat.fr/project/expression-libre/consultation/consultation-9/types/expression-libre-sujets-de-societe",
             "totalCount":3589,
             "pageNb":359
         }
@@ -97,6 +97,8 @@ def extract_by_page(driver, url, page_nb):
     #     pass    
     return map(get_proposal_url, soup.findAll("div", {"class":"opinion__body"})) 
 
+def extract_content_proposal():
+    pass
 
 
 if __name__ == "__main__":
@@ -107,10 +109,12 @@ if __name__ == "__main__":
     driver = webdriver.Firefox(options=options)
     for theme_name, theme in parameters.items():
         print(theme["url"], theme["pageNb"])
-        with open(theme_name+".csv", "w") as f:
+        counter = 0
+        with open(theme_name+"-2.csv", "w") as f:
             w = csv.writer(f)
-            
-            for page_nb in range(1, theme["pageNb"]+1):
-                print([n for n in extract_by_page(driver, theme["url"], page_nb)])
+            offset = theme["totalCount"]//10
+            if theme["totalCount"]%10 > 0:
+                offset +=1
+            for page_nb in range(1, offset+1):
                 w.writerows(extract_by_page(driver, theme["url"], page_nb))
     driver.close()
